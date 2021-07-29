04021bcb8a2018906599d54467e62fa24d01f1a1
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2021-07-29 16:47:15 -0400

    CockroachCloud connection update

--- app/src/main/java/example/app/App.java
+++ app/src/main/java/example/app/App.java
@@ -7,18 +7,17 @@ import org.postgresql.ds.PGSimpleDataSource;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the demo password: ");
-        String password = scan.nextLine();
 
         try {
             PGSimpleDataSource ds = new PGSimpleDataSource();
-            ds.setServerNames(new String[]{"localhost"});
+            ds.setServerNames(new String[]{"<globalhost>"});
             ds.setPortNumbers(new int[]{26257});
-            ds.setUser("demo");
-            ds.setPassword(password);
+            ds.setDatabaseName("<cluster>.defaultdb");
             ds.setSsl(true);
-            ds.setSslMode("require");
+            ds.setUser("<user>>");
+            ds.setPassword("<password>");
+            ds.setSslMode("verify-full");
+            ds.setSslRootCert(System.getenv("$HOME/.postgresql/root.crt"));
             ds.setApplicationName("App");
             System.out.println("Hey! You connected to your CockroachDB cluster.");
         }
