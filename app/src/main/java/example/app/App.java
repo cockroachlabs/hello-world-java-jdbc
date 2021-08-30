379d4b65504024f4c84627eb57bc0937b0f7ede1
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2021-08-30 15:36:17 -0400

    Updated connection parameters for insecure single-node testing; removed init subprocess

--- app/src/main/java/example/app/App.java
+++ app/src/main/java/example/app/App.java
@@ -1,24 +1,19 @@
 package example.app;
 
-import java.util.Scanner;
-import javax.sql.DataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 
 public class App {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the demo password: ");
-        String password = scan.nextLine();
-
+        
         try {
             PGSimpleDataSource ds = new PGSimpleDataSource();
             ds.setServerNames(new String[]{"localhost"});
             ds.setPortNumbers(new int[]{26257});
-            ds.setUser("demo");
-            ds.setPassword(password);
-            ds.setSsl(true);
-            ds.setSslMode("require");
+            ds.setDatabaseName("defaultdb");
+            ds.setUser("root");
+            ds.setSsl(false);
+            ds.setSslMode("disable");
             ds.setApplicationName("App");
             System.out.println("Hey! You connected to your CockroachDB cluster.");
         }
